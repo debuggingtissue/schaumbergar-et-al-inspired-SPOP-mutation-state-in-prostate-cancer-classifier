43960594bcbd63a91df842f4cfc2386d5f5f20ea
debuggingtissue <debugger@debuggingtissue.com>
2020-05-26 00:15:18 +0200

    add dataset split method

--- src/0a_train_simple_convnet.py
+++ src/0a_train_simple_convnet.py
@@ -1,17 +1,30 @@
 from pathlib import Path
 from utils.dataset_generator import *
 from utils.constants import *
+import torch
 
 if __name__ == '__main__':
     CURRENT_PATH = Path.cwd()
     SPOP_FALSE_DATA_PATH = CURRENT_PATH.parent / "data" / SPOP_FALSE
     SPOP_TRUE_DATA_PATH = CURRENT_PATH.parent / "data" / SPOP_TRUE
 
+    TRAIN_PROPORTION = 0.7
+    VALIDATION_PROPORTION = 0.1
+    TEST_PROPORTION = 0.2
+
     image_data_paths = [SPOP_FALSE_DATA_PATH, SPOP_TRUE_DATA_PATH]
-    dataset = create_image_dataset_from_image_data_paths(image_data_paths)
+    full_dataset = create_image_dataset_from_image_data_paths(image_data_paths)
+    train_set, validation_set, test_set = split_into_train_validation_test_sets(full_dataset, TRAIN_PROPORTION,
+                                                                                VALIDATION_PROPORTION, TEST_PROPORTION)
 
 
-    print(dataset)
+    print(len(full_dataset))
+    print(len(train_set))
+    print(len(validation_set))
+    print(len(test_set))
+
+
+
     # print(SPOP_FALSE_DATA_PATH)
     # print(SPOP_TRUE_DATA_PATH)
 
