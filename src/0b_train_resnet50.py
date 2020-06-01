3954ad809e62544d5477b2f8ef408d6c19c12bc5
debuggingtissue <debugger@debuggingtissue.com>
2020-06-01 14:19:57 +0200

    clean up dependencies

--- src/0b_train_resnet50.py
+++ src/0b_train_resnet50.py
@@ -4,11 +4,6 @@
 from __future__ import print_function, division
 
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from torchvision import datasets, models, transforms
-from pathlib import Path
 from utils import ensemble_manager, path_utils
 
 if __name__ == '__main__':
